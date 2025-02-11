4da323eaa5aa2f62ff194adbf5d6cbcc0e07fd12
ImRayy <souradeep_roy@pm.me>
2025-02-12 00:40:02 +0530

    refactor(ui): enhance custom text component
    - better ui
    - better editing experience
    - smooth animations

--- src/components/widgets/custom-text.tsx
+++ src/components/widgets/custom-text.tsx
@@ -1,69 +1,82 @@
 import clsx from "clsx"
 import { useEffect, useRef, useState } from "react"
-import { useThemeStore } from "~/store/theme"
 import { useOptionsStore } from "../../store/options"
 import { getGreetings } from "../../utils/datetime"
 import { parseDate } from "../../utils/datetime"
-import Input from "../ui/input"
 
 const date = new Date()
 
 const getFirstWords = (str: string) => str.split("").splice(0, 3).join("")
+const textPlaceHolder = "Click here to edit"
 
 export const CustomText = () => {
   const { customText, setCustomText } = useOptionsStore()
-  const { enableDigitalClock: isDigitalClockEnabled, greetings } =
-    useOptionsStore()
-  const isLightMode = useThemeStore((s) => s.isLightMode)
+  const {
+    enableDigitalClock: isDigitalClockEnabled,
+    greetings,
+    isBgImage,
+  } = useOptionsStore()
 
   const [openInput, setOpenInput] = useState(false)
 
-  const inputRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+  const parentRef = useRef<HTMLDivElement>(null)
+  const hiddenSpanRef = useRef<HTMLSpanElement>(null)
+
   const { day, month, weekDay } = parseDate(date)
 
   useEffect(() => {
     openInput && inputRef.current?.focus()
   }, [openInput])
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.style.height = "auto"
+      inputRef.current.style.height = `${inputRef.current.scrollHeight}px`
+
+      if (parentRef.current && hiddenSpanRef.current) {
+        const parentWidth = parentRef.current.scrollWidth
+        const textWidth = hiddenSpanRef.current.scrollWidth
+
+        const newWidth = Math.min(Math.max(textWidth + 40, 50), parentWidth)
+        inputRef.current.style.width = `${newWidth}px`
+      }
+    }
+  }, [customText])
+
   return (
-    <div className="flex w-full flex-col items-start text-foreground text-lg">
-      {openInput ? (
-        <Input
-          ref={inputRef}
-          id="custom-text"
-          placeholder="Type here"
-          value={customText}
-          onInput={(e) => setCustomText(e.currentTarget.value)}
-          outline="ghost"
-          className={clsx(
-            "w-full rounded-none bg-inherit px-0 py-0 text-foreground text-xl",
-            isLightMode ? "text-shadow-light" : "text-shadow-dark",
-          )}
-        />
-      ) : (
-        <button
-          type="button"
-          className={clsx(
-            ["text-start text-xl"],
-            isLightMode ? "text-shadow-light" : "text-shadow-dark",
-          )}
-          onClick={() => setOpenInput(true)}
-        >
-          {customText || "Click here to edit"}
-        </button>
+    <div
+      ref={parentRef}
+      className={clsx(
+        "flex w-full flex-col items-start text-foreground text-lg",
+        isBgImage && "space-y-2",
       )}
+    >
+      <span className="invisible" ref={hiddenSpanRef}>
+        {customText || textPlaceHolder}
+      </span>
+      <textarea
+        ref={inputRef}
+        id="custom-text"
+        placeholder={textPlaceHolder}
+        value={customText}
+        onInput={(e) => setCustomText(e.currentTarget.value)}
+        onBlur={() => setOpenInput(false)}
+        spellCheck={false}
+        rows={1}
+        className={clsx(
+          "resize-none overflow-hidden rounded-xl border-none bg-card px-2 py-1 text-foreground text-lg transition-all placeholder:text-foreground focus:border-none focus:ring-0",
+        )}
+      />
       {isDigitalClockEnabled ? (
         greetings && (
-          <span
-            className={isLightMode ? "text-shadow-light" : "text-shadow-dark"}
-          >
+          <span className="rounded-xl bg-card px-2 py-1">
             {getGreetings(date)}
           </span>
         )
       ) : (
-        <span
-          className={isLightMode ? "text-shadow-light" : "text-shadow-dark"}
-        >{`${getFirstWords(weekDay)}, ${getFirstWords(month)} ${day}`}</span>
+        <span className="rounded-xl bg-card px-2 py-1">{`${getFirstWords(weekDay)}, ${getFirstWords(month)} ${day}`}</span>
       )}
     </div>
   )
